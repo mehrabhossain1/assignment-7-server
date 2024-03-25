161398e0e3fcd4800cae782049719cdded30cb45
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-25 23:36:24 +0600

    live url in origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://splendorous-piroshki-b84b3d.netlify.app",
     credentials: true,
   })
 );
